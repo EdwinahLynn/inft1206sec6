ca315d0a6e538cbb9f01ed1480bfe12fd1a883fe
EdwinahLynn <lynnedwinah@gmail.com>
2024-07-15 12:05:34 -0400

    assignment4pt1.html
    I changed the formula

--- assignments/assignment4/assignment4pt1.js
+++ assignments/assignment4/assignment4pt1.js
@@ -34,7 +34,7 @@ function result() {
   }
 
   if(document.getElementById("uk").checked) {
-    const weight = Math.round(300/2.2) + " stone";
+    const weight = Math.round(300/14) + " stone";
     const temperature =  Math.round((94-32)*(5/9)) + " centigrade";
     
     newStory = newStory.replace("300 pounds", weight)
