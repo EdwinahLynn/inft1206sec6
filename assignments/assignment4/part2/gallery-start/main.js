c1b582f69742fbc9f1973033f21809ae8179b817
EdwinahLynn <lynnedwinah@gmail.com>
2024-07-23 10:10:45 -0400

    assignment4pt2.html update
    I added the array of images and alternative texts, and I created a for loop that loops through them

--- assignments/assignment4/part2/gallery-start/main.js
+++ assignments/assignment4/part2/gallery-start/main.js
@@ -5,14 +5,18 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
+const imageFiles = ["./images/pic1.jpg", "./images/pic2.jpg", "./images/pic3.jpg","./images/pic4.jpg","./images/pic5.jpg",]
+//const imageFiles = [pic1.jpg, pic2.jpg, pic3.jpg, pic4.jpg, pic5.jpg,]
+
 
 /* Declaring the alternative text for each image file */
+const textAlts = ["one eye", "water", "purple and white flowers", "acient egyptian drawings ", "a butterfly"]
 
 /* Looping through images */
-
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
-thumbBar.appendChild(newImage);
+for (let i = 0; i <= imageFiles.length-1; i++){
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src', imageFiles[i]);
+    newImage.setAttribute('alt', textAlts[i]);
+    thumbBar.appendChild(newImage);
 
 /* Wiring up the Darken/Lighten button */
