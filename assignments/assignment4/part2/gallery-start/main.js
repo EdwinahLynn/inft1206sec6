b96b66d2fc666ec78eb269a1be9c516896832f34
EdwinahLynn <lynnedwinah@gmail.com>
2024-07-25 11:16:42 -0400

    assignment4pt2.html update
    Made final changes

--- assignments/assignment4/part2/gallery-start/main.js
+++ assignments/assignment4/part2/gallery-start/main.js
@@ -6,7 +6,7 @@ const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
 const imageFiles = ["./images/pic1.jpg", "./images/pic2.jpg", "./images/pic3.jpg","./images/pic4.jpg","./images/pic5.jpg",]
-//const imageFiles = [pic1.jpg, pic2.jpg, pic3.jpg, pic4.jpg, pic5.jpg,]
+
 
 
 /* Declaring the alternative text for each image file */
