673ab57fed508689ea770d8d6f51d42386bd7365
EdwinahLynn <lynnedwinah@gmail.com>
2024-07-29 10:44:56 -0400

    assignment4pt4 update
    I added a sync function

--- assignments/assignment4/part4/assignment4pt4.js
+++ assignments/assignment4/part4/assignment4pt4.js
@@ -13,10 +13,14 @@ const aliceTumbling = [
   const alice2 = document.querySelector("#alice2");
   const alice3 = document.querySelector("#alice3");
 
-  const firstAnimation = alice1.animate(aliceTumbling, aliceTiming);
-  
-firstAnimation.finished
-    .then(alice2.animate(aliceTumbling, aliceTiming)
-    .then(alice3.animate(aliceTumbling, aliceTiming)
-  
-));
+
+  async function forAnimation ()  {
+    const firstAnimation = alice1.animate(aliceTumbling, aliceTiming);
+    await firstAnimation.finished
+
+    const secondAnimation = alice2.animate(aliceTumbling, aliceTiming)
+    await secondAnimation.finished
+
+    const thirdAnimation = alice3.animate(aliceTumbling, aliceTiming)
+    await thirdAnimation.finished
+};
